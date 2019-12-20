4aa94bbefd8f02f6c796c89f42f759952acfc2c6
Kishor9552 <kishorsapkal18@gmail.com>
2019-12-20 12:04:21 +0530

    ***********

---
+++ src/cccc/Test_cccc.java
@@ -0,0 +1,31 @@
+package cccc;
+
+public class Test_cccc {
+ 
+	public static void main(String[] args) {
+		System.out.println("eclipse massege");
+		System.out.println("github massege");
+		Test_cccc tc=new Test_cccc();
+		tc.addition();
+		tc.multiplication();
+		
+		
+
+	}
+	public static int addition() {
+		int a=10;
+		int b=20;
+		int sum=a+b;
+		System.out.println(sum);
+		return sum;
+	}
+	public static int multiplication(){
+		int a=20;
+		int b=30;
+		int product=a*b;
+	 System.out.println(product);
+		return product;
+			
+
+}
+}
